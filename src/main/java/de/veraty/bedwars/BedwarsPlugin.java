376902c2dc3ec7b84423878c2fd79ccb4a587bf1
veraty <veratydev@gmail.com>
2017-11-26 18:09:56 +0100

    + Added BedwarsPlugin#getUsable() BedwarsPlugin#getClickable()

--- src/main/java/de/veraty/bedwars/BedwarsPlugin.java
+++ src/main/java/de/veraty/bedwars/BedwarsPlugin.java
@@ -16,23 +16,37 @@
  */
 package de.veraty.bedwars;
 
+import de.veraty.bedwars.inventory.Clickable;
+import de.veraty.bedwars.inventory.Usable;
+import de.veraty.bedwars.player.PlayerWrapper;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Getter;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  *
  * @author Merlin
  */
+@Getter
 public class BedwarsPlugin extends JavaPlugin {
 
     @Getter
     private static BedwarsPlugin instance;
+
+    private final List<Usable> usables;
+    private final List<Clickable> clickables;
 
     /**
      * Constructs a BedwarsPlugin
      */
     public BedwarsPlugin() {
         instance = this;
+
+        this.usables = new ArrayList<>();
+        this.clickables = new ArrayList<>();
     }
 
     @Override
@@ -43,5 +57,40 @@ public class BedwarsPlugin extends JavaPlugin {
     private void initialize() {
 
     }
+
+    /**
+     * Gets an usable
+     *
+     * @param item
+     * @param playerWrapper
+     * @return usable
+     */
+    public Usable getUsable(ItemStack item, PlayerWrapper playerWrapper) {
+        for (int i = 0; i < usables.size(); i++) {
+            if (usables.get(i).matches(item, playerWrapper)) {
+                return usables.get(i);
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Gets a clickable
+     *
+     * @param inventory
+     * @param playerWrapper
+     * @return clickable
+     */
+    public Clickable getClickable(Inventory inventory, PlayerWrapper playerWrapper) {
+        for (int i = 0; i < clickables.size(); i++) {
+            if (clickables.get(i).matches(inventory, playerWrapper)) {
+                return clickables.get(i);
+            }
+        }
+        return null;
+    }
+    
+    
+    
 
 }
